d89d8e4d9bd245956d7677eb8255763bf9f86e5b
macakstefan <macak.m.stefan@gmail.com>
2024-11-27 04:04:48 +0100

    debug mechanism

--- Solution/cv_to_human_vision.py
+++ Solution/cv_to_human_vision.py
@@ -1,17 +1,18 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 class CVToHumanVision():
-    def __init__(self, image_path, roi, new_dim):
-        self.image_path = image_path
-        self.image = cv2.imread(image_path)
+    def __init__(self, roi, new_dim, debug=False):
         self.roi = roi
         self.new_dim = new_dim
+        self.debug = debug
         
 
-    def draw_lines(self, poly_left, poly_right, img):
+    def draw_lines(self, poly_left, poly_right, image):
         # Generate y-values
+        img = copy.deepcopy(image)
         y_values = np.linspace(0, img.shape[0] - 1, img.shape[0])
 
         left_x_values = poly_left[0] * y_values**2 + poly_left[1] * y_values + poly_left[2]
@@ -29,11 +30,11 @@ class CVToHumanVision():
             cv2.line(img, (right_x_values[i], y_values[i]), (right_x_values[i + 1], y_values[i + 1]), (0, 0, 255), 20)
 
         # Display the image
-        cv2.imshow('Lanes', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if self.debug:
+            cv2.imshow('Lanes', img)
 
-    def draw_lines_on_original_image(self, img, poly_left, poly_right):
+
+    def draw_lines_on_original_image(self, img, poly_left, poly_right, original_image):
     
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         y_values = np.linspace(0, img.shape[0] - 1, img.shape[0])
@@ -58,10 +59,8 @@ class CVToHumanVision():
         cv2.fillPoly(polygon_img, np.int_([points]), (255, 255, 0))
 
         M_inv = cv2.getPerspectiveTransform(self.new_dim, self.roi)
-        warped_polygon = cv2.warpPerspective(polygon_img, M_inv, (img.shape[1], img.shape[0]))
-        result = cv2.addWeighted(self.image, 1, warped_polygon, 0.3, 0)
+        warped_polygon = cv2.warpPerspective(polygon_img, M_inv, (original_image.shape[1], original_image.shape[0]))
+        result = cv2.addWeighted(original_image, 1, warped_polygon, 0.3, 0)
 
-        # Display the image
+        # # # Display the image
         cv2.imshow('Lane Lines', result)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
