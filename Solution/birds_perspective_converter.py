c4c6773814227a40e6629d6d401a49cb2f1beb62
macakstefan <macak.m.stefan@gmail.com>
2024-11-27 04:02:58 +0100

    wider trapezoid change.

--- Solution/birds_perspective_converter.py
+++ Solution/birds_perspective_converter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import copy
 
 
 class BirdsPerspectiveConverter:
@@ -13,30 +14,37 @@ class BirdsPerspectiveConverter:
         result = None
         roi = self.generate_roi()
         #write new dim based on picture size
+
+         # Calculate the width and height of the trapezoid
+        top_width = np.linalg.norm(np.array(roi[0]) - np.array(roi[1]))
+        bottom_width = np.linalg.norm(np.array(roi[2]) - np.array(roi[3]))
+        height = np.linalg.norm(np.array(roi[0]) - np.array(roi[2]))
+
+        # Set new_dim to fit the entire trapezoid
         new_dim = np.float32([[0, 0],
-                                    [self.img_width, 0],
-                                    [0, self.img_height],
-                                    [self.img_width, self.img_height]])
+                              [max(top_width, bottom_width), 0],
+                              [0, height],
+                              [max(top_width, bottom_width), height]])
         
         M = cv2.getPerspectiveTransform(roi, new_dim)
-        result = cv2.warpPerspective(self.img, M, self.img_size, flags=cv2.INTER_LINEAR)
+        result = cv2.warpPerspective(self.img, M, (int(max(top_width, bottom_width)), int(height)), flags=cv2.INTER_LINEAR)
 
         if(self.debug):
             cv2.imshow('Birds perspective', result)
 
-        return result
+        return result, roi, new_dim
 
 
     def generate_roi(self):
         # Calculate ROI coordinates as a trapezoid on the left side of the image
-        top_left = (int(self.img_width * 0.45), int(self.img_height * 0.62))
-        top_right = (int(self.img_width * 0.55), int(self.img_height * 0.62))
-        bottom_left = (int(self.img_width * 0.15), int(self.img_height * 0.9))
-        bottom_right = (int(self.img_width * 0.95), int(self.img_height * 0.9))
+        top_left = (int(self.img_width * 0.40), int(self.img_height * 0.66))
+        top_right = (int(self.img_width * 0.62), int(self.img_height * 0.66))
+        bottom_left = (int(self.img_width * 0.07), int(self.img_height * 0.98))
+        bottom_right = (int(self.img_width * 0.97), int(self.img_height * 0.98))
 
         #draw roi on image
         if(self.debug):
-            roi_on_img = self.img.copy()
+            roi_on_img = copy.deepcopy(self.img)
             roi_on_img = cv2.cvtColor(roi_on_img, cv2.COLOR_GRAY2RGB)
             cv2.line(roi_on_img, top_left, top_right, (0, 255, 0), 2)
             cv2.line(roi_on_img, top_right, bottom_right, (0, 255, 0), 2)
