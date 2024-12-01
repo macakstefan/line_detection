17902169579beb2db9c663709f093af102861afd
macakstefan <macak.m.stefan@gmail.com>
2024-12-01 23:55:00 +0100

    cleanup

--- Solution/birds_perspective_converter.py
+++ Solution/birds_perspective_converter.py
@@ -4,16 +4,15 @@ import copy
 
 
 class BirdsPerspectiveConverter:
-    def __init__(self, img, use_image, debug=False):
+    def __init__(self, img,original_img, debug=False):
         self.img = img
+        self.original_img = original_img
         self.img_size = (img.shape[1], img.shape[0])
         self.img_height, self.img_width = img.shape[:2]
         self.debug = debug
-        self.use_image = use_image
 
     def transform_to_birds_perspective(self):
         result = None
-        #write new dim based on picture size
 
         roi = self.generate_roi()
 
@@ -38,15 +37,13 @@ class BirdsPerspectiveConverter:
 
 
     def generate_roi(self):
-        # Calculate ROI coordinates as a trapezoid 
-        # Calculate ROI coordinates as a trapezoid on the left side of the image
         top_left = (int(self.img_width * 0.40), int(self.img_height * 0.66))
         top_right = (int(self.img_width * 0.62), int(self.img_height * 0.66))
         bottom_left = (int(self.img_width * 0.07), int(self.img_height * 0.98))
         bottom_right = (int(self.img_width * 0.97), int(self.img_height * 0.98))
 
         if(self.debug):
-            roi_on_img = copy.deepcopy(self.img)
+            roi_on_img = copy.deepcopy(self.original_img)
             # roi_on_img = cv2.cvtColor(roi_on_img, cv2.COLOR_GRAY2RGB)
             bottom_left = tuple(map(int, bottom_left))
             bottom_right = tuple(map(int, bottom_right))
@@ -59,36 +56,7 @@ class BirdsPerspectiveConverter:
             cv2.line(roi_on_img, bottom_left, top_left, (0, 255, 0), 2)
 
             cv2.imshow('Image with ROI', roi_on_img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+
 
         return np.float32([top_left, top_right, bottom_left, bottom_right])
     
-
-    def transform_to_birds_perspective2(self):
-        result = None
-        # Hardcoded trapezoid coordinates
-        roi = np.float32([
-            [200, 720],  # Bottom-left corner
-            [1100, 720],  # Bottom-right corner
-            [595, 450],  # Top-right corner
-            [685, 450]   # Top-left corner
-        ])
-
-        # Hardcoded dimensions for the new perspective
-        new_width = 1000  # Example width
-        new_height = 800  # Example height
-
-        # Set new_dim to the hardcoded dimensions
-        new_dim = np.float32([[0, 0],
-                            [new_width, 0],
-                            [0, new_height],
-                            [new_width, new_height]])
-
-        M = cv2.getPerspectiveTransform(roi, new_dim)
-        result = cv2.warpPerspective(self.img, M, (new_width, new_height), flags=cv2.INTER_LINEAR)
-
-        if self.debug:
-            cv2.imshow('Birds perspective 2', result)
-
-        return result, roi, new_dim
