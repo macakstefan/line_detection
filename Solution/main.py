e940176a28668204266f35398eb274a4d32dc782
macakstefan <macak.m.stefan@gmail.com>
2024-12-01 14:09:32 +0100

    Quit on camera calibration, after many tries it was impossible to have trapezoid same for all images after calibration.

--- Solution/main.py
+++ Solution/main.py
@@ -1,18 +1,16 @@
 import os
 import numpy as np
-import pickle
 import cv2
 from calibration import ChessboardCameraCalibrator
 from binary_converter import ImageBinaryConverter
-import matplotlib.pyplot as plt
 from lines_detector import LineDetector
 from cv_to_human_vision import CVToHumanVision
 from birds_perspective_converter import BirdsPerspectiveConverter
 
 
 print("Current working dir:", os.getcwd())
-IMAGE_PATH = "test_images/test6.jpg"
-VIDEO_PATH = "C:\\Users\\macak\\Documents\\master\\computer vision\\Zadatak\\test_videos\\project_video02.mp4"
+IMAGE_PATH = "test_images/test1.jpg"
+VIDEO_PATH = "C:\\Users\\macak\\Documents\\master\\computer vision\\Zadatak\\test_videos\\project_video01.mp4"
 # CALIBRATE = True
 CALIBRATE = False
 USE_IMAGE= True
@@ -33,31 +31,35 @@ def do(img, calibrate=False):
                                     'Solution/output_undistorted_images')
 
     
-    img = calibrator.undistort_image('Solution/calibration_data.npz', img)
-    print("Undistorted image shape:", img.shape)
+    original_img = calibrator.undistort_image('Solution/calibration_data.npz', img)
+    # img = calibrator.undistort_image('Solution/calibration_data.npz', img)
+
+    # return
     original_img = img.copy()
+
+    birds_perspective = BirdsPerspectiveConverter(img,USE_IMAGE, DEBUG)
+    img, roi, new_dim, M = birds_perspective.transform_to_birds_perspective()
+    # return
 
 
     #use color transforms, gradients, etc., to create a thresholded binary image
     binary_converter = ImageBinaryConverter()
 
-
     img = binary_converter.convert_image_to_binary(img)
     print("Binary image shape:", img.shape)
-    birds_perspective = BirdsPerspectiveConverter(img,USE_IMAGE, DEBUG)
-    img, roi, new_dim = birds_perspective.transform_to_birds_perspective()
+
 
    
     try:
         ld = LineDetector(img, 20, 10, 10, DEBUG)
-        poly_left, poly_right =ld.detect()      
+        poly_left, poly_right =ld.detect()  
         converter = CVToHumanVision(roi, new_dim, DEBUG)
         converter.draw_lines(poly_left, poly_right, img)
-        converter.draw_lines_on_original_image(img, poly_left, poly_right, original_img)
+        result = converter.draw_lines_on_original_image(img, poly_left, poly_right, original_img, M)
+
     except Exception as e:
         print("Errroror:", e)
         
-    return 
         
 
 if __name__ == '__main__':
@@ -79,9 +81,6 @@ if __name__ == '__main__':
             
             if ret:
                 do(frame, CALIBRATE)
-                # generate_roi(frame)
-                # Display the resulting frame
-                # cv2.imshow('Frame', frame)
 
                 # Add a delay and check for 'q' key to exit
                 if cv2.waitKey(30) & 0xFF == ord('q'):
